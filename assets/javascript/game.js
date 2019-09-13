ba13dd1131f002cc15528f0c084a3d674eb92a85
Charissa Beaty <char.beaty11@gmail.com>
2019-09-13 11:32:55 -0600

    console.log values of each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,52 @@
 //Random number (between 19-120) appears here. Changes at start of new game
+var total = "";
+var wins = 0;
+var losses = 0;
+
+console.log(total);
+var crystalNumber = Math.floor(Math.random() *12) + 1;
+$("#crystal").append(crystalNumber);
+console.log(crystalNumber);
+
 var randomNumber = Math.floor(Math.random() *120) + 19;
 $("#random").text(randomNumber);
 console.log(randomNumber);
 
 //each crystal needs to have a different number (between 1-12) when clicked
-var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-for (var i = 0; i < numberOptions.length; i++) {
 
-var imageCrystal = $("#crystal-image");
+$(document).ready(function() {
+//set crystal values on page load
+var crystal = $("#crystal");
 
-imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
-$("#crystal-image").append(imageCrystal);
+//store values inside teh data attribute of the element
+$("#crystal").attr("data-value", crystalNumber);
+
+
+$(".crystal-image").on('click', function() {
+    
+  var crystalValue = ($(this).attr("data-value"));
+  crystalValue = parseInt(crystalValue);
+
+  var crystalNumber = Math.floor(Math.random() *12) + 1;
+$("#crystal").append(crystalNumber);
+console.log(crystalNumber);
+
+  total += crystalValue;
+
+   if (total === randomNumber){
+   wins++;
+//restart game
+}
+if ( total !== randomNumber){
+losses--;
+//restart game
 }
 
+});
 
-
-var totalScore = 0;
-var wins = 0;
-var losses = 0;
-
-var yourTotal = document.getElementById("your-total");
-var yourWins = document.getElementById("your-wins");
-var yourLosses = document.getElementById("your-losses");
-
-function startGame(){
-    randomNumber.textContent = "";
-    yourTotal.textContent = totalScore;
-    yourWins.textContent = wins;
-    yourLosses.textContent = losses;
-
-}
-
-$("#crystal-image").on("click", function() {
-    //pull from numberOptions to get value
-var crystalValue = ($(this).attr("data-crystalvalue"));
-crystalValue = parseInt(crystalValue);
-
-    totalScore += crystalValue;
-//apply to total score
-if (yourTotal === randomNumber){
-    wins++;
-    startGame();
-}
-if (yourTotal !== randomNumber){
-    losses++;
-    startGame();
-}
-
-randomNumber.textContent = "";
-yourTotal.textContent = totalScore;
-yourWins.textContent = wins;
-yourLosses.textContent = losses;
-})
-//make startGame function
-//total score added here 
-//keep track of losses
-//keep track of wins
+$("#your-total").text(total);
+$("#your-wins").text(wins);
+$("#your-losses").text(losses);
+});
