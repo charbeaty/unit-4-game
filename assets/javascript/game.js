bb83ea94b429ca513457c9df6ebcf208e4ebb834
Charissa Beaty <char.beaty11@gmail.com>
2019-09-14 18:17:59 -0600

    added jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,57 @@
-//Random number (between 19-120) appears here. Changes at start of new game
-var total = "";
+
+var total = 0;
 var wins = 0;
 var losses = 0;
-
-console.log(total);
-var crystalNumber = Math.floor(Math.random() *12) + 1;
-$("#crystal").append(crystalNumber);
-console.log(crystalNumber);
-
-var randomNumber = Math.floor(Math.random() *120) + 19;
-$("#random").text(randomNumber);
-console.log(randomNumber);
-
-//each crystal needs to have a different number (between 1-12) when clicked
-
-
-$(document).ready(function() {
-//set crystal values on page load
-var crystal = $("#crystal");
-
-
-//store values inside teh data attribute of the element
-$("#crystal").attr("data-value", crystalNumber);
-
-
-$(".crystal-image").on('click', function() {
-    
-  var crystalValue = ($(this).attr("data-value"));
-  crystalValue = parseInt(crystalValue);
-
-  var crystalNumber = Math.floor(Math.random() *12) + 1;
-$("#crystal").append(crystalNumber);
-console.log(crystalNumber);
-
-  total += crystalValue;
-
-   if (total === randomNumber){
-   wins++;
-//restart game
-}
-if ( total !== randomNumber){
-losses--;
-//restart game
-}
-
-});
 
 $("#your-total").text(total);
 $("#your-wins").text(wins);
 $("#your-losses").text(losses);
+
+var crystal = $("#crystal");
+var crystalNumber = Math.floor(Math.random() * 12) + 1;
+
+
+var randomNumber = Math.floor(Math.random() * 120) + 19;
+$("#random").text(randomNumber);
+console.log(randomNumber);
+
+
+
+
+$(document).ready(function () {
+
+    //store values inside the data attribute of the element
+    $("#crystal").attr("data-value", crystalNumber);
+
+
+    $(".crystal-image").on('click', function () {
+
+        var crystalNumber = parseInt($(this).attr("data-crystalNumber"));
+        
+
+        total += crystalNumber;
+
+        var crystalNumber = Math.floor(Math.random() * 12) + 1;
+        $("#crystal").append(crystalNumber);
+        console.log(crystalNumber);
+
+
+
+        if (total === randomNumber) {
+            wins++;
+            total++;
+            //restart game
+        }
+        else if (total >= randomNumber) {
+            losses++;
+            total++;
+            //restart game
+        }
+
+    });
+
+    $("#your-total").text(total);
+    $("#your-wins").text(wins);
+    $("#your-losses").text(losses);
+
 });
